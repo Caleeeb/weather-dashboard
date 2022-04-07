90ff763072e34ab590d3a3dade1d04878fc23510
Caleeeb <daycinematography@gmail.com>
2022-04-07 10:27:53 -0400

    script pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+// WEATHER DASHBOARD
+// variables setup
+
+// Assigning a unique API to a variable
+
+// a function that when the user enters a city in the search bar it creates the url for the weather forcast
+    // after url is formed, fetch data
+    // parse the promise reponse to display current weather for given city
+    // current date (day/month/year)
+    // weather icon of what the weather is like
+    // remove any previous data that might have been displayed previously
+    // fetch temperature, humidity, wind speed, uv index
+    // temperature
+    // humidity
+    // wind speed
+// uv index. I'll need to add a color attribute that matches the severity of UV index (maybe use 'if' statement?)
+    // create specific URL for UV Index?
+
+// fetch the five day forecast data
+    // five day specific url?
+    // parse promise reponse for five day forecast data display
+    // icons for the five days
+
+// set city search history in local storage
+// function with a loop that creates a list of clickable text-box element with previously searched cities
+    // include a event listener that when they are clicked, page loads that city's weather data
+
+
