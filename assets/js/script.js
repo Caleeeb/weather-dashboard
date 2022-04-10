580062cff7b6946f6ac1d9fec90d7324cec6bbf3
Caleeeb <daycinematography@gmail.com>
2022-04-10 10:38:42 -0400

    api fetch setup, html structuring for future dom manipulation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,37 @@
 // WEATHER DASHBOARD
 // variables setup
+// dom selector variables
+let cityEl = document.getElementById("city-input");
+let searchEl = document.getElementById("search-button");
 
-// Assigning a unique API to a variable
+
+
+// Assigning a unique API key to a variable
+const APIKey = "3a91f7f0ab2106256c3b3aafbbd9cd58";
 
 // a function that when the user enters a city in the search bar it creates the url for the weather forcast
+function createWeather(cityName) {
+    console.log(cityName);
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+    fetch(weatherURL)
+        .then(function (response) {
+            // testing output
+            response.json().then(function (result) {
+                // date variables
+                const currentDate = new Date(result.dt * 1000);
+                const day = currentDate.getDate();
+                const month = currentDate.getMonth() + 1;
+                const year = currentDate.getFullYear();
+                console.log(currentDate, day, month, year);
+            });
+        });
+}
+// function to retrieve city name on button click.
+searchEl.addEventListener("click", function () {
+    const searchInput = cityEl.value;
+
+    createWeather(searchInput);
+});
     // after url is formed, fetch data
     // parse the promise reponse to display current weather for given city
     // current date (day/month/year)
